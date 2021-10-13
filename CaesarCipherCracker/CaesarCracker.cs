1ed8314274e580e17b9ea2a63d64e6297b097d7d
Nick <nickmartin1ee7@gmail.com>
2021-10-12 20:51:51 -0400

    Refactor classes to separate files

---
+++ CaesarCipherCracker/CaesarCracker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipherCracker
+{
+    internal class CaesarCracker : IDisposable
+    {
+        private const int ALPHA_LENTH = 26;
+        private const string DICT_BASE_URL = "http://www.dict.org/bin/Dict?Form=Dict2&Database=*&Query=";
+        private readonly HttpClient _client = new HttpClient();
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+        }
+
+        public async Task<Solution> CrackAsync(string input)
+        {
+            var solution = new Solution();
+
+            for (int i = 0; i < ALPHA_LENTH; i++)
+            {
+                var currentWordSet = GetWordSetFromKey(input, i);
+                await FindActualWords(currentWordSet, solution);
+            }
+
+            return solution;
+        }
+
+        private string[] GetWordSetFromKey(string text, int key)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in text)
+            {
+                if (!char.IsLetter(c))
+                {
+                    sb.Append(c);
+                    continue;
+                }
+                
+                var newChar = c + key > 'Z'
+                    ? (char)(c - 'Z' + key + 'A' - 1)
+                    : (char)(c + key);
+                
+                sb.Append(newChar);
+            }
+            
+            return sb.ToString()
+                .Split(" ");
+        }
+
+        private async Task FindActualWords(string[] words, Solution solution)
+        {
+            var wordDict = new Dictionary<string, bool>();
+
+            foreach (var word in words)
+            {
+                var httpResponseMessage = await _client.GetAsync(DICT_BASE_URL + word);
+                var content = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                wordDict.Add(word, !content.Contains("No definitions found"));
+            }
+
+            solution.AddWordSet(wordDict);
+        }
+    }
+}
---
+++ CaesarCipherCracker/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaesarCipherCracker
+{
+    public class Solution
+    {
+        private const int ALPHA_LENTH = 26;
+        private List<Dictionary<string, bool>> _keySets = new();
+        
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            // F
+            int indexOfBest = _keySets.IndexOf(_keySets
+                .OrderByDescending(k =>
+                    k.Count(s => s.Value))
+                .First());
+
+            for (int i = 0; i < ALPHA_LENTH; i++)
+            {
+                sb.Append($"[ ] {i}/{ALPHA_LENTH} - ");
+                foreach (var keySet in _keySets[i])
+                {
+                    sb.Append(keySet.Key + " ");
+                }
+
+                if (i == indexOfBest)
+                {
+                    sb.Append("(POSSIBLE SOLUTION)");
+                }
+                
+                sb.AppendLine();
+            }
+
+            if (indexOfBest == -1)
+                sb.AppendLine("[-] No solution found");
+            else
+                sb.AppendLine($"[+] Possible solution is key of {indexOfBest}");
+            
+            return sb.ToString();
+        }
+
+        public void AddWordSet(Dictionary<string, bool> wordDict)
+        {
+            _keySets.Add(wordDict);
+        }
+    }
+}
